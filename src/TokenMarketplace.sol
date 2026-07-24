6e619a86a1b1f68a88c10e9c6a536c1a1e686ca6
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2026-07-24 12:36:02 +0530

    test.yml

--- src/TokenMarketplace.sol
+++ src/TokenMarketplace.sol
@@ -15,7 +15,7 @@ contract TokenMarketplace is Ownable, Pausable, ReentrancyGuard {
     uint256 private constant TOKEN_PRICE = 1 ether;
     uint256 private reseverdOrderedTokens;
     uint256 private nextOrderId;
-    bool dummy = true;
+    
 
     mapping(uint256 => OrderInfo) private orders;
 
@@ -45,7 +45,7 @@ contract TokenMarketplace is Ownable, Pausable, ReentrancyGuard {
         _revertIfZeroTokenAmount(numberOfTokens);
         _revertIfIncorrectEthPayment(numberOfTokens);
         _revertIfTokenBalanceOfMarketplaceIsLow(numberOfTokens);
-        dummy = false;
+        
 
         slvToken.safeTransfer(msg.sender, numberOfTokens);
 
